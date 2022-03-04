c04caa53f0bb740c1b29e0ad23c426a409833974
Nick Kannenberg <nrk5987@rit.edu>
2022-03-04 13:56:48 -0500

    Added state based code to GameStateManager, added section for Runi to work on menu

--- FinalProject/FinalProject/GameStateManager.cs
+++ FinalProject/FinalProject/GameStateManager.cs
@@ -30,17 +30,60 @@ namespace FinalProject
         //Constructors
         public GameStateManager()
         {
-
+            //Set intro timer
+            introTimer = 2f;
         }
 
         //Methods
         public void Display(SpriteBatch batch)
         {
+            //Current state actions
+            switch (currentState)
+            {
+                case GameState.IntroState:
 
+                    break;
+                case GameState.MenuState:
+                    //Runi's Menu Display Here
+
+                    break;
+                case GameState.PlayState:
+
+                    break;
+                case GameState.PauseState:
+
+                    break;
+                case GameState.GameOverState:
+
+                    break;
+            }
         }
         public void Update(float dTime)
         {
+            //Current state actions
+            switch (currentState)
+            {
+                case GameState.IntroState:
+                    introTimer -= dTime;
+                    if(introTimer <= 0)
+                    {
+                        currentState = GameState.MenuState;
+                    }
+                    break;
+                case GameState.MenuState:
+                    //Runi's Menu Work here
 
+                    break;
+                case GameState.PlayState:
+
+                    break;
+                case GameState.PauseState:
+
+                    break;
+                case GameState.GameOverState:
+
+                    break;
+            }
         }
     }
 }
