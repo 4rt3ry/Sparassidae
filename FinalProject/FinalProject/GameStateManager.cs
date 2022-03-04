eb226cd764c8a4c953949c7ded13744546b3df90
Nick Kannenberg <nrk5987@rit.edu>
2022-03-04 13:24:55 -0500

    Set up basics of GameStateManager

--- FinalProject/FinalProject/GameStateManager.cs
+++ FinalProject/FinalProject/GameStateManager.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿/*
+ * Game State Manager class
+ * Handles game states and transitions
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FinalProject
 {
+    enum GameState
+    {
+        IntroState,
+        MenuState,
+        PlayState,
+        PauseState,
+        GameOverState
+    }
     class GameStateManager
     {
+        //Fields
+        private GameState currentState;
+        private float introTimer;
+
+        //Properties
+        public GameState CurrentState { get => currentState; } 
+
+        //Constructors
+        public GameStateManager()
+        {
+
+        }
+
+        //Methods
+        public void Update(float dTime)
+        {
+
+        }
     }
 }
