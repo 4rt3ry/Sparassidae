de08d0565f710ba0404bd3b585a229e378d0d0f5
Nick Kannenberg <nrk5987@rit.edu>
2022-03-04 14:06:48 -0500

    Added state machine to Enemy class

--- FinalProject/FinalProject/Enemy.cs
+++ FinalProject/FinalProject/Enemy.cs
@@ -4,6 +4,7 @@
  * including Enemy State Machine
  */
 
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,17 +13,80 @@ namespace FinalProject
 {
     enum EnemyState
     {
-
+        RoamingState,
+        InvestigateState,
+        ChaseWindupState,
+        ChaseState,
+        PlayerDeadState,
+        ReturnState
     }
     class Enemy
     {
         //Fields
+        private EnemyState currentState;
 
         //Properties
+        public EnemyState CurrentState { get => currentState;  }
 
         //Constructors
 
-        //Methods
 
+        //Methods
+        /// <summary>
+        /// Handles all visual displays for enemy
+        /// </summary>
+        /// <param name="batch">Sprite batch</param>
+        public void Display(SpriteBatch batch)
+        {
+            switch (currentState)
+            {
+                case EnemyState.RoamingState:
+
+                    break;
+                case EnemyState.InvestigateState:
+
+                    break;
+                case EnemyState.ChaseWindupState:
+
+                    break;
+                case EnemyState.ChaseState:
+
+                    break;
+                case EnemyState.PlayerDeadState:
+
+                    break;
+                case EnemyState.ReturnState:
+
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles all time-based functions for enemy
+        /// </summary>
+        /// <param name="dTime">Time passed (Seconds)</param>
+        public void Update(float dTime)
+        {
+            switch (currentState)
+            {
+                case EnemyState.RoamingState:
+
+                    break;
+                case EnemyState.InvestigateState:
+
+                    break;
+                case EnemyState.ChaseWindupState:
+
+                    break;
+                case EnemyState.ChaseState:
+
+                    break;
+                case EnemyState.PlayerDeadState:
+
+                    break;
+                case EnemyState.ReturnState:
+
+                    break;
+            }
     }
 }
