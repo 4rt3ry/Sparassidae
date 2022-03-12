d849dd0c5ebedf7c3420449146ae22d35cf9b306
apowers131 <arthurpow131@gmail.com>
2022-03-12 11:44:51 -0500

    Create Image Effects class for image masking

---
+++ FinalProject/FinalProject/Content/Image_Mask.hlsl
@@ -0,0 +1,56 @@
+﻿// References: https://learn-monogame.github.io/tutorial/first-shader/
+//             https://gamedev.stackexchange.com/questions/38118/best-way-to-mask-2d-sprites-in-xna
+
+#if OPENGL
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0
+#define PS_SHADERMODEL ps_4_0
+#endif
+
+sampler BaseTexture : register(s0);
+
+Texture2D Mask;
+sampler MaskTexture {
+    Texture = ( Mask );
+    addressU = Wrap;
+    addressV = Wrap;
+};
+
+// Mask/Base Texture Parameters
+float MaskLocationX;
+float MaskLocationY;
+float MaskWidth;
+float MaskHeight;
+float BaseTextureWidth;
+float BaseTextureHeight;
+
+struct VertexInput {
+    float4 Position : POSITION0;
+    float4 Color : COLOR0;
+    float4 TexCoord : TEXCOORD0;
+};
+struct PixelInput {
+    float4 Position : SV_Position0;
+    float4 Color : COLOR0;
+    float4 TexCoord : TEXCOORD0;
+};
+
+float4 SpritePixelShader(PixelInput p) : COLOR0 {
+    //We need to calculate where in terms of percentage to sample from the MaskTexture
+    float maskPixelX = p.TexCoord.x * BaseTextureWidth;
+    float maskPixelY = p.TexCoord.y * BaseTextureHeight;
+    float2 maskCoord = float2((maskPixelX - MaskLocationX) / MaskWidth, (maskPixelY - MaskLocationY) / MaskHeight);
+    float4 bitMask = tex2D(MaskTexture, maskCoord);
+
+    float4 tex = tex2D(BaseTexture, p.TexCoord.xy);
+
+    return tex * float4(bitMask.r, bitMask.r, bitMask.r, bitMask.r);
+}
+
+technique SpriteBatch {
+    pass {
+        PixelShader = compile PS_SHADERMODEL SpritePixelShader();
+    }
+}
---
+++ FinalProject/FinalProject/ImageEffects.cs
@@ -0,0 +1,70 @@
+﻿
+// Author: Arthur Powers: 3/12/2022
+// Purpose: 
+//      Handles all shader effects
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FinalProject
+{
+    static class ImageEffects
+    {
+
+        private static Effect _maskEffect;
+
+        /// <summary>
+        /// Must be invoked in order for image effects to work
+        /// </summary>
+        /// <param name="maskEffect"></param>
+        public static void LoadContent(Effect maskEffect)
+        {
+            _maskEffect = maskEffect;
+        }
+
+        /// <summary>
+        /// Draws a masked portion of a base texture
+        /// </summary>
+        /// <param name="batch"></param>
+        /// <param name="texture"></param>
+        /// <param name="mask"></param>
+        /// <param name="maskLocation"></param>
+        /// <param name="destination"></param>
+        private static void DrawImageMask(SpriteBatch batch, Texture2D texture, Texture2D mask, Vector2 maskLocation, Vector2 destination)
+        {
+            DrawImageMask(batch, texture, mask, maskLocation, new Vector2(mask.Width, mask.Height), destination);
+        }
+
+        /// <summary>
+        /// Draws a masked portion of a base texture
+        /// </summary>
+        /// <param name="batch"></param>
+        /// <param name="texture"></param>
+        /// <param name="mask"></param>
+        /// <param name="maskSize"></param>
+        /// <param name="maskLocation"></param>
+        /// <param name="destination"></param>
+        private static void DrawImageMask(SpriteBatch batch, Texture2D texture, Texture2D mask, Vector2 maskSize, Vector2 maskLocation, Vector2 destination)
+        {
+            if (_maskEffect == null) return;
+
+            _maskEffect.Parameters["Mask"].SetValue(mask);
+            _maskEffect.Parameters["MaskLocationX"].SetValue(maskLocation.X);
+            _maskEffect.Parameters["MaskLocationY"].SetValue(maskLocation.Y);
+            _maskEffect.Parameters["MaskWidth"].SetValue(maskSize.X);
+            _maskEffect.Parameters["MaskHeight"].SetValue(maskSize.Y);
+            _maskEffect.Parameters["BaseTextureWidth"].SetValue((float)texture.Width);
+            _maskEffect.Parameters["BaseTextureHeight"].SetValue((float)texture.Height);
+
+            batch.Begin(effect: _maskEffect, samplerState: SamplerState.PointClamp);
+
+            batch.Draw(texture,
+                new Rectangle((int)(destination.X - maskSize.X / 2), (int)(destination.Y - maskSize.X / 2), (int)maskSize.X, (int)maskSize.Y),
+                new Rectangle((int)maskLocation.X, (int)maskLocation.Y, (int)maskSize.X, (int)maskSize.Y),
+                Color.White);
+
+            batch.End();
+        }
+    }
+}
