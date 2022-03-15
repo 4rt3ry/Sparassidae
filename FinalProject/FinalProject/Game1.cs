9909d4152f524e5ccf20deaacdaeac05499950d9
ciyuz <ciyuz@LAPTOP-A878IKEK>
2022-03-15 03:22:18 -0400

    player movement

--- FinalProject/FinalProject/Game1.cs
+++ FinalProject/FinalProject/Game1.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using System.Diagnostics;
+using System;
 namespace FinalProject
 {
     public class Game1 : Game
@@ -11,6 +12,7 @@ namespace FinalProject
 
         private GameStateManager gameStateManager;
 
+        Player playerObject;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -43,10 +45,65 @@ namespace FinalProject
                 Content.Load<Texture2D>("Controls/Options_hover"),
                 Content.Load<Texture2D>("Controls/Play"),
                 Content.Load<Texture2D>("Controls/Play_hover"));
+
+            //creates the player
+            playerObject = new Player();
+
+        }
+        float Lerp(float firstFloat, float secondFloat, float by)
+        {
+            return firstFloat * (1 - by) + secondFloat * by;
+        }
+        Vector2 Lerp(Vector2 firstVector, Vector2 secondVector, float by)
+        {
+            float retX = Lerp(firstVector.X, secondVector.X, by);
+            float retY = Lerp(firstVector.Y, secondVector.Y, by);
+            return new Vector2(retX, retY);
         }
 
-        void PlayerMovement()
+        void PlayerMovement(float dt)
         {
+            float speed = 5;
+            KeyboardState kb = Keyboard.GetState();
+
+            switch (playerObject.CurrentState)
+            {
+                case PlayerState.WalkingState:
+                    speed = 5;
+                    break;
+                case PlayerState.AfraidState:
+                    speed = speed/2;
+                    break;
+                case PlayerState.ShockState:
+                    speed = 1;
+                    break;
+                case PlayerState.ChaseState:
+                    speed *= 2;
+                    break;
+                case PlayerState.DeadState:
+                    speed = 0;
+                    break;
+            }
+
+            //get keyboard inputs
+            bool aDown = kb.IsKeyDown(Keys.A);
+            bool dDown = kb.IsKeyDown(Keys.D);
+            bool wDown = kb.IsKeyDown(Keys.W);
+            bool sDown = kb.IsKeyDown(Keys.S);
+
+
+            Vector2 addVelocity = new Vector2(0, 0);
+            if (aDown) { addVelocity += new Vector2(-speed, 0); }
+            if (dDown) { addVelocity += new Vector2(speed, 0); }
+            if (wDown) { addVelocity += new Vector2(0, speed); }
+            if (sDown) { addVelocity += new Vector2(0, -speed); }
+
+            //adds acceleration by lerping
+            playerObject.Velocity = Lerp(playerObject.Velocity, addVelocity,.1f);
+
+            playerObject.PlayerPosition += playerObject.Velocity;
+            Debug.WriteLine(playerObject.PlayerPosition);
+            Debug.WriteLine("add velocity is " + addVelocity);
 
         }
 
@@ -56,7 +113,7 @@ namespace FinalProject
                 Exit();
 
             gameStateManager.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
-
+            PlayerMovement((float) gameTime.ElapsedGameTime.TotalSeconds);
             base.Update(gameTime);
         }
 
--- FinalProject/FinalProject/Player.cs
+++ FinalProject/FinalProject/Player.cs
@@ -7,6 +7,8 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace FinalProject
 {
@@ -24,8 +26,42 @@ namespace FinalProject
         PlayerState currentState;
 
         //Properties
+        Vector2 playerPosition;
+
+        Vector2 velocity = new Vector2(0, 0);
+        int x;
+        int y;
+        public int X
+        {
+            get => x;
+            set
+            {
+                playerPosition.X = value;
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get => y;
+            set
+            {
+                playerPosition.Y = value;
+                y = value;
+            }
+        }
+
+        public Vector2 PlayerPosition { get => playerPosition; set => playerPosition = value; }
+        public PlayerState CurrentState { get => currentState; set => currentState = value; }
+        public Vector2 Velocity { get => velocity; set => velocity = value; }
 
         //Constructors
+
+        public Player()
+        {
+            playerPosition = new Vector2(0,0);
+        }
+
 
         //Methods
         /// <summary>
@@ -35,7 +71,7 @@ namespace FinalProject
         /// <param name="batch">Sprite batch</param>
         public void Display(SpriteBatch batch)
         {
-            switch (currentState)
+            switch (CurrentState)
             {
                 case PlayerState.WalkingState:
 
@@ -62,7 +98,7 @@ namespace FinalProject
         /// <param name="dTime">Time passed (in seconds)</param>
         public void Update(float dTime)
         {
-            switch (currentState)
+            switch (CurrentState)
             {
                 case PlayerState.WalkingState:
 
