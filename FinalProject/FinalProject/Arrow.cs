9a50e1b7362f6842862e9d7a470c74821eeb0e34
Nick Kannenberg <nrk5987@rit.edu>
2022-04-22 14:47:55 -0400

    Arrows are in, currently not displaying at correct position

--- FinalProject/FinalProject/Arrow.cs
+++ FinalProject/FinalProject/Arrow.cs
@@ -12,6 +12,7 @@ namespace FinalProject
         //Fields
         private Texture2D arrowTexture;
         private Rectangle displayRect;
+        private float rotation;
 
         //Properties
 
@@ -21,17 +22,32 @@ namespace FinalProject
         {
         }
 
-        public Arrow(Vector2 position, Texture2D arrowTexture) : base(position)
+        public Arrow(Vector2 position, Texture2D arrowTexture, String dir) : base(position)
         {
+            rotation = 0;
             this.arrowTexture = arrowTexture;
-            this.displayRect = new Rectangle((int)position.X, (int)position.Y, arrowTexture.Width, arrowTexture.Height);
+            this.displayRect = new Rectangle((int)position.X, (int)position.Y, (int)(arrowTexture.Width/2.5f), (int)(arrowTexture.Height/2.5f));
             _physicsCollider = null;
+            switch (dir)
+            {
+                case "up":
+                    break;
+                case "down":
+                    rotation = (float)Math.PI;
+                    break;
+                case "left":
+                    rotation = (float)((3f / 2f) * Math.PI);
+                    break;
+                case "right":
+                    rotation = (float)(1 / 2f * Math.PI);
+                    break;
+            }
         }
 
         //Methods
-        public void Draw(SpriteBatch sp, Rectangle rect, Color tint)
+        public void Draw(SpriteBatch sp, Color tint)
         {
-            base.Draw(sp, displayRect, arrowTexture, tint);
+            sp.Draw(arrowTexture, new Vector2(displayRect.X, displayRect.Y), null, tint, rotation, new Vector2(arrowTexture.Width / 2, arrowTexture.Height / 2), new Vector2(0.4f, 0.4f), SpriteEffects.None, 0f);
         }
     }
 }
