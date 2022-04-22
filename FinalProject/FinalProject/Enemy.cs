a4642ad7142717f22463a05b76851c8d8dfc437d
Nick Kannenberg <nrk5987@rit.edu>
2022-04-22 14:19:25 -0400

    Fixed frame issue with end game chase

--- FinalProject/FinalProject/Enemy.cs
+++ FinalProject/FinalProject/Enemy.cs
@@ -685,13 +685,13 @@ namespace FinalProject
 
                     // 2. Change speed if wall between enemy and player
                     detectionLink.EndPosition = target.Position;
-                    if(WallDetection())
+                    if (roamDetectionTrigger.Intersects(target.PhysicsCollider))
                     {
                         speed = baseSpeed;
                     }
                     else
                     {
-                        speed = baseSpeed / 2f;
+                        speed = baseSpeed / 2;
                     }
 
                     break;
