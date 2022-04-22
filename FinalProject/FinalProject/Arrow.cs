a232f27aad64c1958f5ce9558b15e8255e073455
Nick Kannenberg <nrk5987@rit.edu>
2022-04-22 14:15:29 -0400

    Started creation of arrow class (Unfinished to examine end game chase bug)

---
+++ FinalProject/FinalProject/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    class Arrow : GameObject
+    {
+
+        //Fields
+        private Texture2D arrowTexture;
+        private Rectangle displayRect;
+
+        //Properties
+
+
+        //Constructors
+        public Arrow()
+        {
+        }
+
+        public Arrow(Vector2 position, Texture2D arrowTexture) : base(position)
+        {
+            this.arrowTexture = arrowTexture;
+            this.displayRect = new Rectangle((int)position.X, (int)position.Y, arrowTexture.Width, arrowTexture.Height);
+            _physicsCollider = null;
+        }
+
+        //Methods
+        public void Draw(SpriteBatch sp, Rectangle rect, Color tint)
+        {
+            base.Draw(sp, displayRect, arrowTexture, tint);
+        }
+    }
+}
